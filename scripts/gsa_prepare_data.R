13495caf02f7ff965bbcbd53e562daedd25669ba
Nuno Fonseca <firstname.lastname@gmail.com>
2016-02-02 16:37:42 +0000

    cleanup

--- scripts/gsa_prepare_data.R
+++ scripts/gsa_prepare_data.R
@@ -2,8 +2,6 @@
 #; -*- mode: R;-*-
 # =========================================================
 #
-# This file is part of iRAP.
-#
 # This is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
