5c8b9f0600b5775a33bfadafd0aecb36332b12dd
Nuno Fonseca <firstname.lastname@gmail.com>
2016-02-03 21:19:04 +0000

    Cleanup. Added debug option

--- scripts/gsa_prepare_data.R
+++ scripts/gsa_prepare_data.R
@@ -89,13 +89,19 @@ perror <- function(...) {
   cat("[ERROR] ",...,"\n",file=stderr())
 }
 
+debug <- FALSE
+pdebug <- function(...) {
+  if (debug) 
+    cat("[ERROR] ",...,"\n",file=stderr())
+}
 ###############################################################################
 usage <- "prepare_data.R --tsv-list file --xml-list file --out prefix"
 option_list <- list(
   make_option(c("-c", "--cores"), type="character",default="2",dest="num_cores",help="Number of cores to use ([default %default])"),
   make_option(c("-i", "--tsv-list"), type="character", dest="tsv.list.file", default=NULL,help="File with the list of TSV file names  (gene ids should appear in the first column)"),
   make_option(c("-x", "--xml-list"), type="character", dest="xml.list.file", default=NULL,help="File with the list of XML configuration file names."),
-  make_option(c("-o", "--out"), type="character",default=NULL,dest="out.file",help="Output file name prefix.")
+  make_option(c("-o", "--out"), type="character",default=NULL,dest="out.file",help="Output file name prefix."),
+  make_option(c("-d", "--debug"), ,action="store_true",default=FALSE,dest="debug",help="Enable debug mode (much more verbose).")
 )
 multiple.options <- NULL
 filenames <- c("tsv.list.file","xml.list.file") ;#filenames that must exist (if defined)
@@ -103,6 +109,8 @@ filenames <- c("tsv.list.file","xml.list.file") ;#filenames that must exist (if 
 # check multiple options values
 mandatory <- c("tsv.list.file","xml.list.file","out.file")
 opt <- myParseArgs(usage = usage, option_list=option_list,filenames.exist=filenames,multiple.options=multiple.options,mandatory=mandatory)
+
+debug <- opt$debug
 
 pinfo("Parameters parsed.")
 for  ( n in names(opt)) {
@@ -143,7 +151,7 @@ if ( length(xml.files) != length(tsv.files) ) {
 # TODO: check for duplicated files
 ################################
 # data
-pvals <- c(0.001,0.01,0.05,0.1)
+pvals <- c(0.01,0.05,0.1)
 gene.name.col <- 1
 # number of significant genes and non-significant genes
 nSigGenes <- list()
@@ -153,8 +161,10 @@ exp2ngenes <- list()
 # Set of genes considered on each experiment
 expgenes <- list()
 
-for ( i in seq(1,length(tsv.files))) {
-  cat(i,"\n")
+nfiles <- length(tsv.files)
+sort.pvals <- sort(pvals)
+for ( i in seq(1,nfiles)) {
+  #cat(i,"\n")
   pinfo("File:",tsv.files[i])
   tsv.df <- load.tsv(tsv.files[i])
   pinfo("File ",tsv.files[i]," loaded.")
@@ -168,7 +178,7 @@ for ( i in seq(1,length(tsv.files))) {
     ## TODO: how to deal with the NAs?? -> pvalue=1
     
     key=paste(exp,":::",cont,sep="")
-    pinfo("Contrast: ", cont)
+    #pinfo("Contrast: ", cont)
     pinfo("key: ", key)
     # create a matrix for each contrast
     sigGenes[[key]] <- list()
@@ -178,37 +188,51 @@ for ( i in seq(1,length(tsv.files))) {
     exp2ngenes[[key]] <- nrow(tsv.df)
     expgenes[[key]] <- as.character(tsv.df[,gene.name.col])
     pinfo("Number of genes: ",exp2ngenes[[key]])
-    cat("PVALS:",as.character(pvals))
-    print(sigGenes[[key]])
-    for (pval in sort(pvals)) {
+    #cat("PVALS:",as.character(pvals))
+    #print(sigGenes[[key]])
+    for (pval in sort.pvals) {
       # TODO: optimize memory usage
-      pinfo("pval:",pval)
-      pinfo(df.col)
+      pdebug("pval:",pval)
+      #pinfo(df.col)
       #sg <- sum(tsv.df[,df.col]<=pval,na.rm=TRUE)
       sg <- tsv.df[tsv.df[,df.col]<=pval,gene.name.col]
       # exclude NA
       sg <- as.character(sg[!is.na(sg)])
       nsigGenes <- length(sg)
-      pinfo("sigGenes: ",sg)
+      pdebug("sigGenes: ",sg)
       #save.image("test.Rdata")
       #load("test.Rdata")
-      pinfo("nsigGenes: ",nsigGenes)
-      pinfo(names(sigGenes[[key]]))
+      pdebug("nsigGenes: ",nsigGenes)
+      #pinfo(names(sigGenes[[key]]))
       sigGenes[[key]][[as.character(pval)]] <- sg
-      pinfo(".")
       nSigGenes[[key]][as.character(pval)] <- nsigGenes
-      pinfo(".")
     }
     names(sigGenes[[key]]) <- as.character(pvals)    
-    pinfo(".")
   }
+  pinfo(round(i/nfiles*100,2),"% done")
 }
 
-print(sigGenes)
-print(exp2ngenes)
-save(nSigGenes,sigGenes,exp2ngenes,expgenes,file=opt$out.file)
+#print(sigGenes)
+#print(exp2ngenes)
+pinfo("Saving to file ",opt$out.file)
+save(pvals,nSigGenes,sigGenes,exp2ngenes,expgenes,file=opt$out.file)
 # Make a few plots
 # Distribution of the number of genes per experiment
 #
 pinfo("That's all folks!")
 q(status=0)
+
+install.packages("XML")
+library(XML)
+x<-xmlToDataFrame("test_1/E-GEOD-3076-configuration.xml")
+x
+doc <- xmlTreeParse("test_1/E-GEOD-3076-configuration.xml", useInternalNodes = TRUE)
+doc
+dd = getNodeSet(doc, "//contrasts")
+dd
+xmlToDataFrame(dd)
+xmlToDataFrame(nodes=dd,homogeneous=T)
+help(xmlToDataFrame)
+doc2 <- xmlTreeParse("test_1/E-GEOD-3076-configuration.xml")
+doc2$doc$children$configuration
+sapply(dd, xmlGetAttr, "id")
